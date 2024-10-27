0c60c0122c726aa56a5168aaf03210a0b20770f1
unknown <ysjopk@gmail.com>
2024-10-27 22:59:55 +0800

    fix: fix bug and adjust condition

--- EasyFengchuanhua/Program.cs
+++ EasyFengchuanhua/Program.cs
@@ -6,6 +6,7 @@ using CommB1;
 using System.Collections.Generic;
 using Google.Protobuf;
 using System.Linq;
+using SharpDX;
 
 
 namespace EasyFengchuanhua
@@ -14,7 +15,7 @@ namespace EasyFengchuanhua
     public class EasyFengchuanhua : ICSharpMod
     {
         public string Name => nameof (EasyFengchuanhua);
-        public string Version => "0.0.1";
+        public string Version => "0.0.2";
         private readonly Harmony harmony;
 
         private static string FOCUS_LEVEL_0 = "棍势等级0";
@@ -23,13 +24,46 @@ namespace EasyFengchuanhua
         private static string FOCUS_LEVEL_3 = "棍势等级3";
         private static string FOCUS_LEVEL_4 = "棍势等级4";
         private static string FOCUS_LEVEL_5 = "棍势等级5";
-        // not changed after restart, not sure if will changed with version
-        private static string FOCUS_NODE_GUID = "f653ea0a-c632-497f-90b7-106a0674c188"; 
+
+        private static bool edgesCached;
+        private static FCalliopeEdge Focus0Edge;
+        private static FCalliopeEdge Focus1Edge;
+        private static FCalliopeEdge Focus2Edge;
+        private static FCalliopeEdge Focus3Edge;
+
 
         public EasyFengchuanhua()
         {
             harmony = new Harmony(Name);
             Harmony.DEBUG = false;
+        }
+
+        void resetFocusEdges()
+        {
+            Focus0Edge = null;
+            Focus1Edge = null;
+            Focus2Edge = null;
+            Focus3Edge = null;
+            edgesCached = false;
+            MyUtils.Log("Edges reset.");
+        }
+
+        static void cacheFocusEdges(
+            FCalliopeEdge value0,
+            FCalliopeEdge value1,
+            FCalliopeEdge value2,
+            FCalliopeEdge value3
+            )
+        {
+            if(!edgesCached)
+            {
+                Focus0Edge = value0;
+                Focus1Edge = value1;
+                Focus2Edge = value2;
+                Focus3Edge = value3;
+                edgesCached = true;
+                MyUtils.Log("Edges cached.");
+            }
         }
 
         void ICSharpMod.Init()
@@ -38,13 +72,14 @@ namespace EasyFengchuanhua
             
             // hook
             this.harmony.PatchAll(Assembly.GetExecutingAssembly());
+            resetFocusEdges();
         }
 
         void ICSharpMod.DeInit()
         {           
             this.harmony.UnpatchAll();
+            resetFocusEdges();
             MyUtils.Log($"===================={Name} DeInit====================");
-
         }
 
         private static T GetNodeCustomData<T>(FCalliopeNode Node) where T : IMessage, new()
@@ -60,72 +95,89 @@ namespace EasyFengchuanhua
         private static bool PrintOnExecuteEdge(ref BUS_PlayerInputActionComp __instance, FCalliopeEdge Edge)
         {
  
-            //if(Edge.To.NodeGuid.ToString() == FOCUS_NODE_GUID) 
-            //{
-                MyUtils.Log($"OnExecuteEdge: {Edge.From.NodeGuid} --> {Edge.To.NodeGuid}");
-                List<string> toKeys = new List<string>(Edge.To.OutputEdges.Keys);
+            MyUtils.Log($"OnExecuteEdge: {Edge.From.NodeGuid} --> {Edge.To.NodeGuid}");
+            List<string> toKeys = new List<string>(Edge.To.OutputEdges.Keys);
                 
 
-                if (toKeys.Contains(FOCUS_LEVEL_0)
-                    && toKeys.Contains(FOCUS_LEVEL_1)
-                    && toKeys.Contains(FOCUS_LEVEL_2)
-                    && toKeys.Contains(FOCUS_LEVEL_3)
-                    && toKeys.Contains(FOCUS_LEVEL_4)
-                    && toKeys.Contains(FOCUS_LEVEL_5))
+            if (toKeys.Contains(FOCUS_LEVEL_0)
+                && toKeys.Contains(FOCUS_LEVEL_1)
+                && toKeys.Contains(FOCUS_LEVEL_2)
+                && toKeys.Contains(FOCUS_LEVEL_3)
+                && toKeys.Contains(FOCUS_LEVEL_4)
+                && toKeys.Contains(FOCUS_LEVEL_5))
+            {
+                toKeys.ForEach(key =>
                 {
+                    MyUtils.Log($"ToNode outputEdge's to: {key} / {Edge.To.OutputEdges[key].To.NodeGuid.ToString()}");
+                });
 
-                    toKeys.ForEach(key =>
-                    {
-                        MyUtils.Log($"ToNode outputEdge's to: {key} / {Edge.To.OutputEdges[key].To.NodeGuid.ToString()}");
-                    });
+                FCalliopeEdge focus4Edge = Edge.To.OutputEdges[FOCUS_LEVEL_4];
 
-                    FCalliopeEdge focus4Edge = Edge.To.OutputEdges[FOCUS_LEVEL_4];
-
-                    List<string> nodeKeys = new List<string>(focus4Edge.To.OutputEdges.Keys);
+                List<string> nodeKeys = new List<string>(focus4Edge.To.OutputEdges.Keys);
 
 
-                    if (nodeKeys.Contains("凤穿花窗口"))
-                    {
-                        MyUtils.Log("Found Fengchuanhua !");
+                if (nodeKeys.Contains("凤穿花窗口"))
+                {
+                    MyUtils.Log("Found Fengchuanhua !");
 
-                        List<CalliopeCustom_ComboCondition> list = GetNodeCustomData<ComboCustom_Start>(Edge.To).ComboConditions.ToList<CalliopeCustom_ComboCondition>();
+                    cacheFocusEdges(
+                        Edge.To.OutputEdges[FOCUS_LEVEL_0],
+                        Edge.To.OutputEdges[FOCUS_LEVEL_1],
+                        Edge.To.OutputEdges[FOCUS_LEVEL_2],
+                        Edge.To.OutputEdges[FOCUS_LEVEL_3]
+                        );
 
-                        CalliopeCustom_ComboCondition? matched = null;
-                        for (int index = 0; index < list.Count; ++index) {
-                            CalliopeCustom_ComboCondition ComboCondition = list[index];
-                            bool result = false;
-                            if (__instance != null)
-                            {
-                                #pragma warning disable CS8605
-                                result = (bool) __instance.CallPrivateFunc("IsConditionSuccess", [ComboCondition]);
-                                #pragma warning restore CS8605 
-                            }
+                    List<CalliopeCustom_ComboCondition> list = GetNodeCustomData<ComboCustom_Start>(Edge.To).ComboConditions.ToList<CalliopeCustom_ComboCondition>();
 
-                            if(result)
-                            {
-                                MyUtils.Log($"ComboCondition matched: {ComboCondition.ConditionIdentity}");
-                                matched = ComboCondition;
-                                break;
-                            }
-                        }
-                        if(matched != null)
+                    CalliopeCustom_ComboCondition? matched = null;
+                    for (int index = 0; index < list.Count; ++index) {
+                        CalliopeCustom_ComboCondition ComboCondition = list[index];
+                        bool result = false;
+                        if (__instance != null)
                         {
-                            if(matched.ConditionIdentity == FOCUS_LEVEL_4 || matched.ConditionIdentity == FOCUS_LEVEL_5)
-                            {
-                                MyUtils.Log($"Focus level is greater than 4 - skip patch edge.");
-                            } else
-                            {
-                                Edge.To.OutputEdges[FOCUS_LEVEL_0] = Edge.To.OutputEdges[FOCUS_LEVEL_4];
-                                Edge.To.OutputEdges[FOCUS_LEVEL_1] = Edge.To.OutputEdges[FOCUS_LEVEL_4];
-                                Edge.To.OutputEdges[FOCUS_LEVEL_2] = Edge.To.OutputEdges[FOCUS_LEVEL_4];
-                                Edge.To.OutputEdges[FOCUS_LEVEL_3] = Edge.To.OutputEdges[FOCUS_LEVEL_4];
-                            }
+                            #pragma warning disable CS8605
+                            result = (bool) __instance.CallPrivateFunc("IsConditionSuccess", [ComboCondition]);
+                            #pragma warning restore CS8605 
+                        }
+
+                        if(result)
+                        {
+                            MyUtils.Log($"ComboCondition matched: {ComboCondition.ConditionIdentity}");
+                            matched = ComboCondition;
+                            break;
+                        }
+                    }
+                    if(matched != null)
+                    {
+                        CalliopeCustom_ComboCondition dodgeWin = new CalliopeCustom_ComboCondition();
+                        dodgeWin.UnitState = (int) EBGUUnitState.InDodgeWindow;
+
+                        #pragma warning disable CS8605
+                        bool dodgeState = (bool)__instance.CallPrivateFunc("OnCheckUnitState", [dodgeWin]);
+                        #pragma warning restore CS8605
+
+                        MyUtils.Log($"dodgeState: {dodgeState}");
+
+                        if(!dodgeState || matched.ConditionIdentity == FOCUS_LEVEL_4 || matched.ConditionIdentity == FOCUS_LEVEL_5)
+                        {
+                            MyUtils.Log($"Focus level is greater than 4 or not in dodge - skip patch edge.");
+                            Edge.To.OutputEdges[FOCUS_LEVEL_0] = Focus0Edge;
+                            Edge.To.OutputEdges[FOCUS_LEVEL_1] = Focus1Edge;
+                            Edge.To.OutputEdges[FOCUS_LEVEL_2] = Focus2Edge;
+                            Edge.To.OutputEdges[FOCUS_LEVEL_3] = Focus3Edge;
+                        } else
+                        {
+                            Edge.To.OutputEdges[FOCUS_LEVEL_0] = Edge.To.OutputEdges[FOCUS_LEVEL_4];
+                            Edge.To.OutputEdges[FOCUS_LEVEL_1] = Edge.To.OutputEdges[FOCUS_LEVEL_4];
+                            Edge.To.OutputEdges[FOCUS_LEVEL_2] = Edge.To.OutputEdges[FOCUS_LEVEL_4];
+                            Edge.To.OutputEdges[FOCUS_LEVEL_3] = Edge.To.OutputEdges[FOCUS_LEVEL_4];
                         }
 
                     }
-                }
 
-            //}
+                }
+            }
+
 
             return true;
         }
